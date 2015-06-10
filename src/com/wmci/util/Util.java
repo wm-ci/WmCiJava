c5fa7ea0a6f815b83b3f8b640108a2916de582f3
Carlos Echeverría <carlosechev07@gmail.com>
2015-06-09 23:16:32 -0700

    Update Util.java
    Adding new methods.

--- src/com/wmci/util/Util.java
+++ src/com/wmci/util/Util.java
@@ -31,4 +31,62 @@ public class Util {
 				input
 				;
 	}
+
+	public static String doNotMapNullOrBlanks(String input) {
+
+		/*if(input != null && !input.trim().equals(""))
+			return input.trim();
+		else 
+			return null;*/
+
+		resturn input != null && !input.trim().equals("") ? input.trim() : null;
+	}
+
+	public static String decode(String input, String conditions, String defaultValue) {
+
+			//Main logic 
+		String match = null;
+		int totalComparations = conditions.length/2,
+			i = 0;		
+		
+		while (match == null && i < totalComparations) {
+			String value = conditions[i*2+0];
+			String returnValue = conditions[i*2+1];
+			
+			if(input != null) {
+				//System.out.printf("value[%s] - compare[%s] - [%s]\n", value, returnValue, (input.equals(value)));
+								
+				match = input.equals(value) ? returnValue : null;
+			}
+			else
+				match = input == value ? returnValue : null;
+			
+			i++;
+		}
+		
+		//Return value
+		return	match != null ? 
+				match : 
+				defaultValue //If there are not match on compared values, then return elseValue
+				;
+	}
+
+	public static String buildPairConditionsArray(String input) {
+
+		//Main logic 
+		String[] array = {},
+				 tmp = input == null || input.trim().equals("") ?
+					   new String[]{} :
+					   input.split(",")
+				;
+		array = new String[tmp.length];
+		
+		for (int i = 0; i < tmp.length; i++) {
+			String item = tmp[i];
+			array[i] = item.equals("{null}") ? null : item;
+		}
+		
+		//Return value
+		return  array;
+	}
 }	
